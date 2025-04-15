c95a391b1f6e3519eb4013f906162ca64f6e2a37
Natalia Petrovska <natalia.petrovska87@gmail.com>
2025-04-15 11:37:25 +0200

    Split to functions

--- function.js
+++ function.js
@@ -1,52 +1,67 @@
 function addOne() {
-  let divResult = document.getElementsByClassName("flex-container-output")[0];
-  let buttonMinusOne = document.getElementById("button-minus-one");
-  let currentNumber = Number(divResult.textContent);
-  if (currentNumber > 0) {
-    buttonMinusOne.disabled = false;
-  }
-  let divRoot = document.getElementsByClassName("flex-container-root")[0];
-  let spanFreeShipping = divRoot.getElementsByTagName("span")[0];
-  if (currentNumber >= 9) {
-    if (spanFreeShipping == undefined) {
-      const spanFreeShipping = document.createElement("span");
-      spanFreeShipping.innerText = "You have free shipping";
-      divRoot.appendChild(spanFreeShipping);
-    }
-  }
-  if (currentNumber >= 19) {
-    let buttonPlusOne = document.getElementById("button-plus-one");
-    buttonPlusOne.style.backgroundColor = "red";
-    buttonPlusOne.disabled = true;
-    spanFreeShipping.innerText = "Out of stock";
-  }
-  divResult.textContent = currentNumber + 1;
+  let nextNumber = getCurrentNumber() + 1;
+  setChanges(nextNumber);
 }
 
 function minusOne() {
-  let divResult = document.getElementsByClassName("flex-container-output")[0];
-  let buttonMinusOne = document.getElementById("button-minus-one");
-  let currentNumber = Number(divResult.textContent);
-  if (currentNumber <= 0) {
-    buttonMinusOne.disabled = true;
-  } else {
-    let divRoot = document.getElementsByClassName("flex-container-root")[0];
-    let spanFreeShipping = divRoot.getElementsByTagName("span")[0];
-    if (currentNumber < 11) {
-      if (spanFreeShipping != undefined) {
-        divRoot.removeChild(spanFreeShipping);
-      }
-    }
-    if (currentNumber < 21) {
-      let buttonPlusOne = document.getElementById("button-plus-one");
-      buttonPlusOne.style.backgroundColor = "whitesmoke";
-      buttonPlusOne.disabled = false;
-      spanFreeShipping.innerText = "You have free shipping";
-    }
-    divResult.textContent = currentNumber - 1;
-  }
+  let nextNumber = getCurrentNumber() - 1;
+  setChanges(nextNumber);
 }
 
 function resetZero() {
-  document.getElementsByClassName("flex-container-output")[0].textContent = "0";
+  setChanges(0);
+}
+
+function setChanges(number) {
+  setNextNumber(number);
+  changeMinusButton(number);
+  changePlusButton(number);
+  showSpanElement(number);
+}
+
+function getCurrentNumber() {
+  let divOutput = document.getElementsByClassName("flex-container-output")[0];
+  let number = Number(divOutput.textContent);
+  return number;
+}
+
+function setNextNumber(number) {
+  let divOutput = document.getElementsByClassName("flex-container-output")[0];
+  divOutput.textContent = number;
+}
+
+function changeMinusButton(number) {
+  let buttonMinusOne = document.getElementById("button-minus-one");
+  if (number > 0) {
+    buttonMinusOne.disabled = false;
+  } else {
+    buttonMinusOne.disabled = true;
+  }
+}
+
+function changePlusButton(number) {
+  let buttonPlusOne = document.getElementById("button-plus-one");
+  if (number < 20) {
+    buttonPlusOne.style.backgroundColor = "whitesmoke";
+    buttonPlusOne.disabled = false;
+  } else {
+    buttonPlusOne.style.backgroundColor = "red";
+    buttonPlusOne.disabled = true;
+  }
+}
+
+function showSpanElement(number) {
+  let divRoot = document.getElementsByClassName("flex-container-root")[0];
+  let spanOld = divRoot.getElementsByTagName("span")[0];
+  if (spanOld != undefined) {
+    divRoot.removeChild(spanOld);
+  }
+  if (number >= 10) {
+    const spanNew = document.createElement("span");
+    spanNew.innerText = "You have free shipping";
+    if (number >= 20) {
+      spanNew.innerText = "Out of stock";
+    }
+    divRoot.appendChild(spanNew);
+  }
 }
